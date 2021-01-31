6152ed23347ce2616298cdc0c3d4093276ccee74
acbuynak <acbuynak@gmail.com>
2021-01-30 19:57:43 -0500

    Add Visualizations Support
    Breakout visualization tools from other scripts into single dedicated file.

--- scripts/process_path.py
+++ scripts/process_path.py
@@ -1,32 +1,6 @@
 #!/usr/bin/env python
 
-def plot_prepared_path(d2p):
-  import matplotlib.pyplot as plt
-  from mpl_toolkits.mplot3d import Axes3D
-
-  while True:
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    # Origin
-    ax.scatter(0, 0, 0)  # origin
-
-    # Path Plotted
-    for p in d2p:
-      ax.scatter(p[0], p[1], p[2], marker="x")
-    ax.plot(d2p[:, 0], d2p[:, 1], d2p[:, 2], color='b')
-
-    # Assign Labels
-    ax.set_xlabel('X-Axis')
-    ax.set_ylabel('Y-Axis')
-    ax.set_zlabel('Z-Axis')
-
-    # Display Plot
-    plt.show()
-
-
-
-def prepare_path(path_source, scaling_factor=0.100):
+def prepare_path_transforms_list(path_source, scaling_factor=0.100):
   import csv
   import numpy as np
 
@@ -56,7 +30,10 @@ def main():
   motion_ready_path = prepare_path('tiny_path_soln.csv')
 
   # Generate Visual
-  plot_prepared_path(motion_ready_path)
+  from visualizations import plot_path_vectors, plot_path_transforms
+  plot_path_vectors(path_as_xyz)
+  plot_path_transforms(path_as_tf_matrices)
+
 
 if __name__ == '__main__':
   main()
---
+++ scripts/visualizations.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+#####################################################
+##   Visualizations Support Class                  ##
+##                                                 ##
+##   Construct Visuals                             ##
+##   * Show path in 3D space                       ##
+##                                                 ##
+#####################################################
+
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2021, The Ohio State University
+# Center for Design and Manufacturing Excellence (CDME)
+# The Artificially Intelligent Manufacturing Systems Lab (AIMS)
+# All rights reserved.
+#
+# Author: Adam Buynak
+
+#####################################################
+
+
+def plot_path_vectors(d2p):
+  import matplotlib.pyplot as plt
+  from mpl_toolkits.mplot3d import Axes3D
+
+  fig = plt.figure()
+  ax = fig.add_subplot(111, projection='3d')
+
+  # Origin
+  ax.scatter(0, 0, 0)  # origin
+
+  # Path Plotted
+  for p in d2p:
+    ax.scatter(p[0], p[1], p[2], marker="x")
+  ax.plot(d2p[:, 0], d2p[:, 1], d2p[:, 2], color='b')
+
+  # Assign Labels
+  ax.set_xlabel('X-Axis')
+  ax.set_ylabel('Y-Axis')
+  ax.set_zlabel('Z-Axis')
+
+  # Display Plot
+  plt.show()
+
+  # Close Figure
+  #plt.close()
+
+
+def plot_path_transforms(d2p):
+  import numpy as np
+  import matplotlib.pyplot as plt
+  from mpl_toolkits.mplot3d import Axes3D
+
+  fig = plt.figure()
+  ax = fig.add_subplot(111, projection='3d')
+
+  # Origin
+  ax.scatter(0, 0, 0)  # origin
+
+  # Collect Array of Just Vectors
+  vectors = np.zeros((len(d2p),3))
+  for i, transform in enumerate(d2p):
+    vectors[i,:] = transform[:-1,3]
+  print(vectors)
+
+  # Path Plotted
+  for p in d2p:
+    ax.scatter(p[0,3], p[1,3], p[2,3], marker="x")
+  ax.plot(vectors[:, 0], vectors[:, 1], vectors[:, 2], color='b')
+
+  # Assign Labels
+  ax.set_xlabel('X-Axis')
+  ax.set_ylabel('Y-Axis')
+  ax.set_zlabel('Z-Axis')
+
+  # Display Plot
+  plt.show()
+
+  # Close Figure
+  #plt.close()
